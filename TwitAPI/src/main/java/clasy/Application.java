6532a510032ddd745a68c5c2a4313c88a7b2819b
gegunt <gegunt@gmail.com>
2016-07-04 17:03:55 +0300

    questions

---
+++ TwitAPI/src/main/java/clasy/Application.java
@@ -0,0 +1,18 @@
+package clasy;
+
+/**
+ * Created by Влад on 04.07.2016.
+ */
+
+        import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+        }
+}
