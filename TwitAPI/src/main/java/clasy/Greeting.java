f5605ee89bf3b9bb73b9bdcbf5d70892f7e9d056
gegunt <gegunt@gmail.com>
2016-07-04 15:43:10 +0300

    created first class file

---
+++ TwitAPI/src/main/java/clasy/Greeting.java
@@ -0,0 +1,22 @@
+package clasy;
+
+/**
+ * Created by Влад on 04.07.2016.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
